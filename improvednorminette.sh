9caba924f938629a87be6e660d9250749926b734
Mehdi Boughrarara <59422745+MehdiBytebyByte@users.noreply.github.com>
2024-10-26 02:16:38 +0100

    Update improvednorminette.sh

--- improvednorminette.sh
+++ improvednorminette.sh
@@ -9,6 +9,26 @@ NC="\033[0m"
 
 issue_count=0
 
+print_final_message() {
+    local count=$1
+
+    if (( count == 0 )); then
+        echo -e "${GREEN}🎉 Awesome! No norm errors found! Keep it up!${NC}"
+    elif (( count <= 3 )); then
+        echo -e "${GREEN}😄 Great job! Just a few minor issues (${count})!${NC}"
+    elif (( count <= 7 )); then
+        echo -e "${YELLOW}😬 Uh-oh! You have ${count} errors. Not bad, but let’s improve!${NC}"
+    elif (( count <= 15 )); then
+        echo -e "${RED}😱 Oh no! ${count} norm errors detected! Time to buckle down!${NC}"
+    elif (( count <= 25 )); then
+        echo -e "${RED}🤦‍♂️ Yikes! ${count} errors! You might need a coffee break!${NC}"
+    elif (( count <= 40 )); then
+        echo -e "${RED}💔 Ouch! ${count} errors! This is starting to hurt!${NC}"
+    else
+        echo -e "${RED}👀 Whoa there! ${count} errors! Only god can help you!${NC}"
+    fi
+}
+
 check_files() {
     local files=("$@")
     
@@ -63,4 +83,5 @@ check_files "${files[@]}"
 
 echo -e "${BLUE}All files have been checked.${NC}"
 echo -e "\n${PURPLE}Norminette issues count is = ${issue_count}${NC}"
+print_final_message "$issue_count"
 echo -e "\n${GREEN}Script created by ${RED}\033]8;;https://github.com/MehdiBytebyByte\033\\Mehdi Boughrara\033]8;;${NC}"
