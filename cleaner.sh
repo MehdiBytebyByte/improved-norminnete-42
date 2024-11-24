4b84bd57ff50b07da881553f0f8f64f82c228e8d
Mehdi Boughrarara <59422745+MehdiBytebyByte@users.noreply.github.com>
2024-11-24 20:08:12 +0100

    Update cleaner.sh

--- cleaner.sh
+++ cleaner.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-
-# Use `find` to iterate over all files and directories
-find . -type f -o -type d | while read -r file; do
-    # Skip files ending with .c, .h, or named Makefile
-    if [[ ! $file =~ \.c$ && ! $file =~ \.h$ && $(basename "$file") != "Makefile" ]]; then
-        read -p "Do you want to delete '$file'? (y/n): " response
-        case "$response" in
-            [yY]) 
-                rm -rf "$file"
-                echo "'$file' has been deleted."
-                ;;
-            [nN]) 
-                echo "'$file' was not deleted."
-                ;;
-            *) 
-                echo "Invalid response. Skipping '$file'."
-                ;;
-        esac
+> .gitignore
+for file in *; do
+    if [[ ! $file =~ \.c$ && ! $file =~ \.h$ && $file != "Makefile" ]]; then
+        echo "$file" >> .gitignore
     fi
 done
-
-echo "Cleanup process completed."
+echo ".gitignore updated with non-C source files and non-Makefile entries."
